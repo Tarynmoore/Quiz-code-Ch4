54a0feb39879f7bc31fdab028543966e071a4a65
Taryn Moore <tarynmoore628@gmail.com>
2022-05-11 15:02:37 -0600

    Added little CSS, nothing set in stone. Made it so when I am done with the questions the text box appears for the user to submit their name for their high score. Still have to set the high score to the timer and get the high score displayed.

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
 var start = document.querySelector(".start");
-
-var questionOne = document.querySelector(".question1");
 var startGame = document.querySelector(".hide");
 var countdownEl = document.querySelector("#countdown"); 
 var questionEl = document.querySelector(".question");
 var titleEl = document.querySelector("#title");
 var choicesEl = document.querySelectorAll(".choices");
+var answerEl = document.querySelectorAll(".answer");
+var scoreEl = document.querySelector(".score");
+var userNameEl = document.querySelector(".userName");
+var submitEl = document.querySelector(".submit");
+var displayEl = document.querySelector(".display");
+
+var playerScore = 0;
 
 var questions = [{
-    title: "question1", 
-    choices: ["answer1", "answer2", "answer3", "answer4"],
-    answer: "answer1"
+    title: "Who founded the Red Cross?", 
+    choices: ["Abraham Lincoln", "Carrie Spector", "Oscar Van Rhijn", "Clara Barton"],
+    answer: "Clara Barton"
   }, {
-    title: "question2",
-    choices: ["answer1", "answer2", "answer3", "answer4"],
-    answer: "answer3"
+    title: "Which tool is used to estimate the likelihood of certain genetic outcomes?",
+    choices: ["Stethescope", "Flask", "Punnet Square", "Punnet Rectangle"],
+    answer: "Punnet Square"
   }, {
-      title: "question3",
-      choices: ["answer1", "answer2", "answer3", "answer4"],
-      answer: "answer3"
+      title: "What is the capitol of New Hampshire?",
+      choices: ["Concord", "Dallas", "Phoenix", "Richmond"],
+      answer: "Concord"
   }, {
-      title: "question4",
-      choices: ["answer1", "answer2", "answer3", "answer4"],
-      answer: "answer1"
+      title: "What is the main character in a story called?",
+      choices: ["Antagonist", "Protagonist", "Sonnet", "Main Character"],
+      answer: "Protagonist"
   }]
 
-var timeRemaining = questions.length * 15;
+console.log(questions);
+
+var timeRemaining = questions.length * 10;
 var timerId;
 var index = 0;
 
@@ -40,15 +47,20 @@ function displayQuestions() {
     choicesEl[2].textContent = questions[index].choices[2]
     choicesEl[3].textContent = questions[index].choices[3]
 }
+console.log(displayQuestions);
+
 function nextquestion() {
     index++
     if (index < questions.length) {
         displayQuestions()
     } else {
-        clearInterval(timerId)
+        clearInterval(timerId) 
+        questionEl.classList.add("hide")
+        displayScore()     
+
     }
-    
 }
+
 choicesEl[0].addEventListener("click", nextquestion);
 choicesEl[1].addEventListener("click", nextquestion);
 choicesEl[2].addEventListener("click", nextquestion);
@@ -56,12 +68,47 @@ choicesEl[3].addEventListener("click", nextquestion);
 
 
 
-
 function renderQuestions() {
     timerId = setInterval(countdown, 1000); 
     questionEl. classList.remove("hide")
-    displayQuestions();
+    displayQuestions();    
 }
 
-
 start.addEventListener("click", renderQuestions);
+
+
+function answers() {
+   answerEl[0].textContent = questions[index].answer[0]
+   answerEl[1].textContent = questions[index].answer[1]
+   answerEl[2].textContent = questions[index].answer[2]
+   answerEl[3].textContent = questions[index].answer[3]
+}
+
+function wrongAnswer() {
+    
+} 
+
+console.log(answers);
+
+function highScore() {
+    if (playerScore === answers) {
+    playerScore++
+    } else {
+        playerScore--
+    }
+}
+
+function displayScore() {       
+        scoreEl.classList.remove("hide")
+        displayEl.textContent = "Your Score is " + highScore + "!" 
+        }
+   
+
+
+//    if (questions.length < displayQuestions) {
+//     displayScore();
+
+
+
+
+
